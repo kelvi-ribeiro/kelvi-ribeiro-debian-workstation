7146d9f0c63908d166163088ced3b66eaabc5856
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-11-22 05:31:00 -0300

    Implements question to continue or not

--- main.sh
+++ main.sh
@@ -11,6 +11,25 @@ function showWelcomeMessage() {
   echo -e "\033[32m| E-mail: $EMAIl |\033[0m"
   echo -e "\033[32m+--------------------------------------------------------------+\033[0m\\n"
   echo "Exibir lista de programas...." 
+  read -p "Você tem certeza que fazer isso?? (Y/N)? " -n 1 -r
+  breakLine 1
+  if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    [[ "$0" == "$BASH_SOURCE" ]] && exit 1 || return 1 
+  fi
+  startInstallApps
+
+}
+
+function breakLine() {  
+  for i in $(seq 1 $1); do
+    echo
+  done  
+}
+
+function startInstallApps() {
+  echo -e "\033[32m| Iniciando instalação das aplicações.... |\033[0m"
+  sleep $SLEEP_TIME
+  echo -e "Instalando aplicação do grupo x"
 }
 
 function main() {
