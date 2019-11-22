e5898ffdf6338c4990c196527e50eef3b80089d9
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-11-22 05:14:05 -0300

    Show welcome message when initializing script

--- main.sh
+++ main.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+AUTHOR="Kelvi Ribeiro"
+EMAIl="kelvi.ribeiro@gmail.com"
+SLEEP_TIME=5
+
+function showWelcomeMessage() {
+  echo -e "\033[32m+--------------------------------------------------------------+\033[0m"
+  echo -e "\033[32m| Seja bem Vindo, Usuário $USER |\033[0m"
+  echo -e "\033[32m| Autor: $AUTHOR |\033[0m"
+  echo -e "\033[32m| E-mail: $EMAIl |\033[0m"
+  echo -e "\033[32m+--------------------------------------------------------------+\033[0m\\n"
+  echo "Exibir lista de programas...." 
+}
+
 function main() {
- echo 'Hello world'
+  clear
+  showWelcomeMessage
 }
 
 main
