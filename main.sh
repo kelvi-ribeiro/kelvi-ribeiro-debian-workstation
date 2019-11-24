8a42013597a61f4d339eba2cee7f443255b92dfc
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-11-24 18:35:02 -0300

    Removes enventual apt lock and prepare environment

--- main.sh
+++ main.sh
@@ -10,26 +10,38 @@ function showWelcomeMessage() {
   echo -e "\033[32m| Autor: $AUTHOR |\033[0m"
   echo -e "\033[32m| E-mail: $EMAIl |\033[0m"
   echo -e "\033[32m+--------------------------------------------------------------+\033[0m\\n"
-  echo "Exibir lista de programas...." 
-  read -p "Você tem certeza que fazer isso?? (Y/N)? " -n 1 -r
+  echo "Exibir lista de programas...."
+  read -p "Você tem certeza que fazer isso? (Y/N)? " -n 1 -r
   breakLine 1
   if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-    [[ "$0" == "$BASH_SOURCE" ]] && exit 1 || return 1 
+    [[ "$0" == "$BASH_SOURCE" ]] && exit 1 || return 1
   fi
+  removesEventualAptLocks
+  preparesEnvironment
   startInstallApps
-
 }
 
-function breakLine() {  
+function breakLine() {
   for i in $(seq 1 $1); do
     echo
-  done  
+  done
 }
 
 function startInstallApps() {
   echo -e "\033[32m| Iniciando instalação das aplicações.... |\033[0m"
   sleep $SLEEP_TIME
-  echo -e "Instalando aplicação do grupo x"
+}
+
+function removesEventualAptLocks() {
+  echo -e "Removendo travas eventuais do apt..."
+  sleep $SLEEP_TIME
+  sudo rm /var/lib/dpkg/lock-frontend
+  sudo rm /var/cache/apt/archives/lock
+}
+
+function preparesEnvironment() {
+  echo -e "Preparando ambiente..."
+  sudo apt update  
 }
 
 function main() {
