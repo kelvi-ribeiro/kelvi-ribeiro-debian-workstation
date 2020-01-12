da47bacf4d0c7ff4e481459eebad960e741be961
kelvi-ribeiro <kelvi.ribeiro@gmail.com>
2020-01-12 16:53:07 -0300

    Change installation of node to use repository

--- main.sh
+++ main.sh
@@ -89,8 +89,7 @@ installSnapAndItsApps() {
   sudo snap install spotify
   sudo snap install postman
   sudo snap install dbeaver-ce
-  sudo snap install android-studio --classic
-  sudo snap install node --edge --classic
+  sudo snap install android-studio --classic  
   sudo snap install intellij-idea-community --classic --edge  
   sudo snap install chromium  
   sudo snap install ngrok
@@ -106,15 +105,19 @@ installRepositoryApps() {
   sudo apt install default-jdk -y 
   sudo apt install git -y 
   sudo apt-get install curl -y   
+  sudo apt install nodejs -y
+  sudo apt install npm -y
 }
 
 installManualDownloadedApp(){
   printf "Installing manually downloaded applications..."
+  sleep $MEDIUM_SLEEP_TIME
   installStackDonetCore()
 }
 
 installStackDonetCore(){  
   printf "Installing Stack Dotnet Core"
+  sleep $MEDIUM_SLEEP_TIME
   sudo apt-get install apt-transport-https -y
   sudo apt-get update 
   sudo apt-get install dotnet-sdk-3.1 -y      
